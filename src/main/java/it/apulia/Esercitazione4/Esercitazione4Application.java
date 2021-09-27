971fefaa8b47465cb12d24e7556d8dfa5621496f
Alberto Catania <alberto91catania@gmail.com>
2021-09-27 13:22:48 +0200

    dimenticato di aggiungere bean per il password encoder nel main (cambiata inoltre versione jdk da 17 a 16)

--- src/main/java/it/apulia/Esercitazione4/Esercitazione4Application.java
+++ src/main/java/it/apulia/Esercitazione4/Esercitazione4Application.java
@@ -2,12 +2,20 @@ package it.apulia.Esercitazione4;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class Esercitazione4Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Esercitazione4Application.class, args);
+	}
+
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
